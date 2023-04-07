f1854d6649b5896a64f34620b127eec434d01af9
Zahra Mirzaei <z.mirzaei2019@gmail.com>
2023-04-07 23:18:01 +0330

    feat: get object values

---
+++ typescript/src/02-unions-and-indexing/12-get-object-values.ts
@@ -0,0 +1,22 @@
+import { Equal, Expect } from "../helpers/type-utils";
+
+const frontendToBackendEnumMap = {
+  singleModule: "SINGLE_MODULE",
+  multiModule: "MULTI_MODULE",
+  sharedModule: "SHARED_MODULE",
+} as const;
+
+type BackendModuleEnum =
+  typeof frontendToBackendEnumMap[keyof typeof frontendToBackendEnumMap];
+
+//or
+
+type Obj = typeof frontendToBackendEnumMap;
+
+type BackendModuleEnum2 = Obj[keyof Obj];
+
+type tests = [
+  Expect<
+    Equal<BackendModuleEnum, "SINGLE_MODULE" | "MULTI_MODULE" | "SHARED_MODULE">
+  >
+];
