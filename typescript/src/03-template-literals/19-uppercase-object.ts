2e39ae58dd43c1905a1b6ce1f7c45afecd0f26df
Zahra Mirzaei <z.mirzaei2019@gmail.com>
2023-04-25 23:26:19 +0330

    feat: uppercase object

---
+++ typescript/src/03-template-literals/19-uppercase-object.ts
@@ -0,0 +1,20 @@
+import { Equal, Expect } from "../helpers/type-utils";
+
+type Event = `log_in` | "log_out" | "sign_up";
+
+//Uppercase - Lowercase - Capitalize =>helper
+
+type ObjectOfKeys = Record<Uppercase<Event>, string>;
+
+type tests = [
+  Expect<
+    Equal<
+      ObjectOfKeys,
+      {
+        LOG_IN: string;
+        LOG_OUT: string;
+        SIGN_UP: string;
+      }
+    >
+  >
+];
