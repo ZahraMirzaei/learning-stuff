0512e2e4c1ddd28337d0c671c5a92450fe6ee31b
Zahra Mirzaei <z.mirzaei2019@gmail.com>
2023-05-02 15:10:45 +0330

    feat: multiple - type helper

---
+++ typescript/src/03.5-type-helpers-pattern/20.3-multiple.ts
@@ -0,0 +1,36 @@
+import { Equal, Expect } from "../helpers/type-utils";
+// lesson: 50
+type CreateDataShape<T, K> = {
+  data: T;
+  error: K;
+};
+
+type tests = [
+  Expect<
+    Equal<
+      CreateDataShape<string, TypeError>,
+      {
+        data: string;
+        error: TypeError;
+      }
+    >
+  >,
+  Expect<
+    Equal<
+      CreateDataShape<number, Error>,
+      {
+        data: number;
+        error: Error;
+      }
+    >
+  >,
+  Expect<
+    Equal<
+      CreateDataShape<boolean, SyntaxError>,
+      {
+        data: boolean;
+        error: SyntaxError;
+      }
+    >
+  >
+];
