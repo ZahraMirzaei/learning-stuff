c698006d99a8bb7b32991d64893621f839db7885
Zahra Mirzaei <z.mirzaei2019@gmail.com>
2023-05-02 14:52:43 +0330

    doc: add description on 20-type-helpers-pattern file, to be more clear

--- typescript/src/03.5-type-helpers-pattern/20-type-helpers-pattern.ts
+++ typescript/src/03.5-type-helpers-pattern/20-type-helpers-pattern.ts
@@ -1,6 +1,13 @@
 import { Equal, Expect } from "../helpers/type-utils";
 
 type ReturnWhatIPassIn<T> = T;
+/**
+ * Here ReturnWhatIPassIn is a function and <T> is its argument and T is what is return
+ * if we remove <T> part, it doesn't a function anymore, it just a static type.
+ * We can pass more than ane argument to it, also we can pass in, default values.
+ * It's a type function.
+ * When we use Record<T, F>, it uses this type function and returns {[P in T]: F;}
+ */
 
 type something = ReturnWhatIPassIn<string>;
 
