1843f55294be0a7fc7a5e5f37917cc5aaf7b7eaa
Zahra Mirzaei <z.mirzaei2019@gmail.com>
2023-05-02 15:19:24 +0330

    feat: defaults, set default type for one of the parameter

---
+++ typescript/src/03.5-type-helpers-pattern/20.4-defaults.ts
@@ -0,0 +1,34 @@
+import { Equal, Expect } from "../helpers/type-utils";
+
+type CreateDataShape<TData, TError = undefined> = {
+  data: TData;
+  error: TError;
+};
+
+/**
+ * type CreateDataShape<TData, TError extends Error | undefined = undefined> = {
+ *    data: TData;
+ *    error: TError;
+ * }
+ */
+
+type tests = [
+  Expect<
+    Equal<
+      CreateDataShape<string>,
+      {
+        data: string;
+        error: undefined;
+      }
+    >
+  >,
+  Expect<
+    Equal<
+      CreateDataShape<boolean, SyntaxError>,
+      {
+        data: boolean;
+        error: SyntaxError;
+      }
+    >
+  >
+];
