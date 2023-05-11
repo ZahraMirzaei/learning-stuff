769eccae1b9e60b705b234993d7cae915f3f2344
Zahra Mirzaei <z.mirzaei2019@gmail.com>
2023-05-11 23:02:30 +0330

    doc: fix one mistake in doc

--- typescript/src/03.5-type-helpers-pattern/20-type-helpers-pattern.ts
+++ typescript/src/03.5-type-helpers-pattern/20-type-helpers-pattern.ts
@@ -4,7 +4,7 @@ type ReturnWhatIPassIn<T> = T;
 /**
  * Here ReturnWhatIPassIn is a function and <T> is its argument and T is what is return
  * if we remove <T> part, it doesn't a function anymore, it just a static type.
- * We can pass more than ane argument to it, also we can pass in, default values.
+ * We can pass more than one argument to it, also we can pass in, default values.
  * It's a type function.
  * When we use Record<T, F>, it uses this type function and returns {[P in T]: F;}
  */
